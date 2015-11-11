4f94aa16faa0eccc12358e44892731f37de18b7a
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-11-11 01:19:47 -0500

    rewrote this

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import argparse
 from bs4 import BeautifulSoup
 import json
-import os
 import requests
 import signal
 import sys
@@ -19,119 +18,84 @@ parser.add_argument('-r', '--rooms', dest='rooms', default='AngryToaster',
 args = parser.parse_args()
 
 
-# Global session
-s = None
-
-# Configuration dictionary
-cfg = {}
-
-
-class ChatMessage:
-    def __init__(self, user, text):
-        self.user = user
-        self.test = text
-
-
+# TODO: Logger pls
 def log(txt):
     print('[{0}] {1}'.format(time.strftime('%H:%M:%S'), txt))
 
 
-def login():
-    global s
+class initium_message_client():
+    def __init__(self, cfg):
+        log('Starting session')
+        self.session = requests.session()
+        self.cfg = cfg
+        self.groups = ['global', 'location', 'group', 'party', 'private', 'notification']
+        self.markers = {}
+        for g in self.groups:
+            self.markers[g] = 'null'
 
-    log('Sending Authentication')
-    payload = {'type': 'login',
-               'rtm': '',
-               'email': cfg['email'],
-               'password': cfg['pw']}
+    def marker_str(self):
+        ret = self.markers[self.groups[0]]
+        for g in self.groups[1:]:
+            ret += ',' + self.markers[g]
+        return ret
 
-    login = s.post('https://www.playinitium.com/ServletUserControl', data=payload)
+    def login(self):
+        log('Sending Authentication')
+        payload = {'type': 'login',
+                   'rtm': '',
+                   'email': self.cfg['email'],
+                   'password': self.cfg['pw']}
 
-    if login.status_code != 200:
-        log('Error: Login returned unexpected error code: ' + str(login.status_code) + ': ' + login.reason + ' - ')
-        sys.exit(1)
+        login = self.session.post('https://www.playinitium.com/ServletUserControl', data=payload)
 
+        if login.status_code != 200:
+            log('Error: Login returned unexpected error code: ' + str(login.status_code) + ': ' + login.reason + ' - ')
+            sys.exit(1)
 
-def get_room_chat(roomID, init_marker):
-    global s
-    return s.get('https://www.playinitium.com/chat?roomId={0}&marker={1}'.format(roomID, init_marker))
+        log('Fetching Chat')
+        self.get_chat(init=True)
 
+    def get_chat(self, slack=False, init=False):
+        t = 'https://www.playinitium.com/messager?markers={0}'.format(self.marker_str())
+        # log(t)
+        m = self.session.get(t)
+        if m.status_code != 200:
+            log('Chat is NOT OK right now')
+        for messages, group in zip(m.json(), self.groups):
+            # log('parsing: {}'.format(group))
+            if messages is None:
+                continue
+            self.parse_chat(messages, group, slack)
 
-def init_cfg():
-    global cfg
+    def parse_chat(self, l, group, slack):
+        # TODO:
+        if group != 'global':
+            return
+        if len(l) == 0:
+            return
+        for message in l:
+            name = BeautifulSoup(message['nickname'], 'lxml')
+            msg = BeautifulSoup(message['message'], 'lxml')
+            log('{0}: {1}'.format(name.text, msg.text))
+            if slack:
+                self.slack_msg(name.text, msg.text, group)
+        self.markers[group] = str(message['marker'])
 
-    log('Reading Config')
-    if os.path.isfile(args.config_file):
-        with open(args.config_file, encoding='utf-8') as data_file:
-            cfg = json.loads(data_file.read())
-    else:
-        print('Error: ' + args.config_file + ' not found')
-        sys.exit(1)
+    def slack_msg(self, usr, txt, group):
+        # Create the content we want to send to slack, via incoming webhook
+        payload = {'channel': cfg['slack_channel'][group],
+                   'username': usr,
+                   'text': txt,
+                   'icon_emoji': cfg['slack_icon_emoji']
+                   }
 
-    # Check the config for a "rooms" dictionary
-    if 'rooms' not in cfg:
-        print('Error: The configuration is missing a Room dictionary')
-        print('Example: ', end='\n\n')
-        print('   "rooms": {')
-        print('        "Aera": {')
-        print('            "id": "L5629499534213120"')
-        print('        }')
-        print('    }', end='\n\n')
-        sys.exit(1)
+        # Slack accepts json payloads
+        headers = {'content-type': 'application/json'}
 
-    if 'email' not in cfg:
-        print('Error: "email": is missing in the config')
-        sys.exit(1)
-
-    if 'pw' not in cfg:
-        print('Error: "pw": is missing in the config')
-        sys.exit(1)
-
-    if 'uname' not in cfg:
-        print('Error: "uname": is missing in the config')
-        sys.exit(1)
-
-    if args.slack_enabled and 'slack_channel' not in cfg:
-        print('Error: "slack_channel": is missing in the config')
-        sys.exit(1)
-
-    if args.slack_enabled and 'slack_icon_emoji' not in cfg:
-        print('Error: "slack_icon_emoji": is missing in the config')
-        sys.exit(1)
-
-    if args.slack_enabled and 'slack_url' not in cfg:
-        print('Error: "slack_url": is missing in the config')
-        sys.exit(1)
-
-
-def init_session():
-    global s
-    log('Starting session')
-    s = requests.session()
-
-
-def slack_msg(room, usr, txt):
-    # Create the content we want to send to slack, via incoming webhook
-    payload = {'channel': cfg['slack_channel'][room],
-               'username': usr,
-               'text': txt,
-               'icon_emoji': cfg['slack_icon_emoji']
-               }
-
-    # Slack accepts json payloads
-    headers = {'content-type': 'application/json'}
-
-    # shipit.jpg
-    r = requests.post(cfg['slack_url'], data=json.dumps(payload), headers=headers)
-    if r.status_code != 200:
-        log('Error: ' + str(r.status_code) + ' ' + r.reason + ' - ' + r.text)
-
-
-def update_cfg():
-    global cfg
-    with open(args.config_file, 'w') as f:
-        json.dump(cfg, f, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': '))
-        f.write('\n')
+        # shipit.jpg
+        r = requests.post(cfg['slack_url'], data=json.dumps(payload), headers=headers)
+        if r.status_code != 200:
+            log('Slack Error: ' + str(r.status_code) + ' ' + r.reason + ' - ' + r.text)
 
 
 def keyboard_interrupt_handler(signal, frame):
@@ -141,97 +105,29 @@ def keyboard_interrupt_handler(signal, frame):
 
 
 def sighup_handler(signal, frame):
-    init_session()
-    login()
+    # init_session()
+    # login()
     log('Configuration reset')
 
 
-def parse_rooms(raw_rooms):
-    global cfg
-    ret = []
-
-    for raw_room in raw_rooms.split(','):
-        raw_room = raw_room.strip()
-        if raw_room in cfg['rooms']:
-            if 'id' not in cfg['rooms'][raw_room]:
-                print('Error: ' + raw_room + ' is missing it\'s "id": tag')
-                sys.exit(1)
-            if args.slack_enabled and raw_room not in cfg['slack_channel']:
-                print('Error: "slack_channel": does not have an entry for "' + raw_room + '":')
-                sys.exit(1)
-            ret.append(raw_room)
-        else:
-            print('Error: ' + raw_room + ' missing')
-            sys.exit(1)
-    return ret
+def save_cfg(filename, cfg):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(cfg), sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, keyboard_interrupt_handler)
     signal.signal(signal.SIGHUP, sighup_handler)
-    init_cfg()
-    init_session()
 
-    rooms = parse_rooms(args.rooms)
+    with open(args.config_file) as f:
+        cfg = json.loads(f.read())
 
-    for room in rooms:
-        if 'marker' in cfg['rooms'][room]:
-            log('[' + room + '] Found marker: ' + str(cfg['rooms'][room]['marker']))
-        else:
-            marker = str(int(time.time() * 1000))
-            log('[' + room + '] Marker was missing, using epoch: ' + marker)
+    print(cfg)
 
-            cfg['rooms'][room]['marker'] = marker
-            update_cfg()
-
-    login()
-    log('Initialization complete.')
-    log('Monitoring: ' + str(rooms))
-    try:
-        while True:
-            try:
-                for room in rooms:
-                    response = get_room_chat(cfg['rooms'][room]['id'], cfg['rooms'][room]['marker'])
-                    # print(str(response.status_code) + ' ' + response.reason)
-                    if response.text:
-                        content = json.loads(response.text)
-                        soup = BeautifulSoup(content['messages'], 'html.parser')
-
-                        for_later = []
-                        for e in soup.find_all('span'):
-                            if 'chatMessage-time' in e['class']:
-                                msg = ChatMessage('', '')
-                            if 'chatMessage-nickname' in e['class']:
-                                msg.user = e.text.strip()
-                            if 'chatMessage-text' in e['class']:
-                                if not msg.user:
-                                    msg.user = e.text.strip()
-                                else:
-                                    msg.text = e.text.strip()
-                                    for_later.insert(0, msg)
-
-                        for msg in for_later:
-                            if msg.text == '':
-                                # Hacks
-                                msg.text = ' '
-
-                            log('[' + room + '] ' + msg.user + ': ' + msg.text)
-                            # Post a message to slack (if it's enabled at startup with --slack)
-                            if args.slack_enabled:
-                                slack_msg(room, msg.user, msg.text)
-
-                        cfg['rooms'][room]['marker'] = str(content['marker'])
-                        update_cfg()
-
-                time.sleep(10)
-
-            # This happens after a while
-            except ConnectionResetError:
-                # Reload configuration
-                sighup_handler()
-
-    # TODO: This might not actually happen ...
-    except ConnectionRefusedError as e:
-        log('Connection was lost')
+    I = initium_message_client(cfg)
+    I.login()
+    while(1):
+        time.sleep(5)
+        I.get_chat(slack=True)
 
     sys.exit(0)
